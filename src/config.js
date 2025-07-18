097d7584b185d145021d48957c05015c7b590724
5Sean <54045068+5SeanSean@users.noreply.github.com>
2025-07-18 17:10:23 -0500

    l

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const geoApiKey = "5c3d59298a0a4add90a4235799a9e76c";
+export const geoApiEndpoint = "https://api.ipgeolocation.io/ipgeo";
+export const fallback = "https://ipgeolocation.abstractapi.com/v1/?api_key=e5bc23d5479f4689b0f4f480367e3b31&ip_address=2601:240:c981:1940:add2:1d84:d5cb:90d9"
+//i know you can see this 😭
