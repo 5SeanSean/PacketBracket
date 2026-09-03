d2b985c00875bf8ee23ec483f750bfe55a1aa00a
5Sean <54045068+5SeanSean@users.noreply.github.com>
2026-09-03 14:15:44 -0500

    added icon
    added icon

--- electron/main.js
+++ electron/main.js
@@ -59,6 +59,7 @@ function createWindow() {
     minHeight: 600,
     backgroundColor: '#0a0a0a',
     title: 'PacketBracket',
+    icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
